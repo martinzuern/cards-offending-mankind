8fe9f916b0cf332e853c679bfcf05bdf8b6bc012
Martin Zürn <hi@martinzuern.de>
2020-05-06 00:13:21 +0200

    Migrated tests to jest

--- backend/__tests__/packs.controller.ts
+++ backend/__tests__/packs.controller.ts
@@ -1,16 +1,15 @@
-import 'mocha';
-import { expect } from 'chai';
 import request from 'supertest';
 import Server from '../server';
 
 describe('Packs', () => {
-  it('should get all packs', () =>
+  it('should get all packs', (done) =>
     request(Server)
       .get('/api/v1/packs')
       .expect('Content-Type', /json/)
       .then((r) => {
-        expect(r.body).to.be.an('array').of.length(72);
-        expect(r.body[0]).to.eql({
+        expect(r.body).toBeInstanceOf(Array);
+        expect(r.body).toHaveLength(72);
+        expect(r.body[0]).toEqual({
           abbr: 'BaseUK',
           description: 'The UK edition of the base game',
           icon: '',
@@ -19,5 +18,6 @@ describe('Packs', () => {
           promptsCount: 90,
           responsesCount: 460,
         });
+        done();
       }));
 });
