8e134a4f75809e197ffca00e125f1e0400e08ab9
Martin Zürn <hi@martinzuern.de>
2020-05-05 23:21:36 +0200

    Added sockets to load; removed example controller

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,6 +1,7 @@
 import './common/env';
 import Server from './common/server';
 import routes from './routes';
+import sockets from './sockets';
 
 const port = parseInt(process.env.PORT);
-export default new Server().router(routes).listen(port);
+export default new Server().router(routes).socket(sockets).listen(port);
--- backend/server/routes.ts
+++ backend/server/routes.ts
@@ -1,9 +1,7 @@
 import { Application } from 'express';
-import examplesRouter from './api/controllers/examples/router';
 import packsRouter from './api/controllers/packs/router';
 import gamesRouter from './api/controllers/games/router';
 export default function routes(app: Application): void {
-  app.use('/api/v1/examples', examplesRouter);
   app.use('/api/v1/packs', packsRouter);
   app.use('/api/v1/games', gamesRouter);
 }
