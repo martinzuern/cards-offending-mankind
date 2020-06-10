4f843237c7645e93b26089abe0c1bd930996e4fd
Martin Zürn <hi@martinzuern.de>
2020-06-10 22:13:24 +0200

    Updated tests for new version of json-against-humanity

--- backend/__tests__/packs.controller.ts
+++ backend/__tests__/packs.controller.ts
@@ -16,16 +16,8 @@ describe('Packs', () => {
       .expect('Content-Type', /json/)
       .expect((r) => {
         expect(r.body).toBeInstanceOf(Array);
-        expect(r.body).toHaveLength(72);
-        expect(r.body[0]).toEqual({
-          abbr: 'BaseUK',
-          description: 'The UK edition of the base game',
-          icon: '',
-          name: 'Base Set (UK Edition)',
-          official: true,
-          promptsCount: 90,
-          responsesCount: 460,
-        });
+        expect(r.body).toHaveLength(99);
+        expect(r.body[0]).toMatchSnapshot();
       })
       .end(done));
 });
