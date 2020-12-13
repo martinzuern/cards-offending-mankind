2771fe1731ce492107856cf0b0f6c1a08be36415
Martin Zürn <hi@martinzuern.de>
2020-12-13 18:43:33 +0100

    Changed game ended screen

--- frontend/src/components/AccessGame/GameState.vue
+++ frontend/src/components/AccessGame/GameState.vue
@@ -2,7 +2,9 @@
   <div v-if="game && player">
     <!-- Game ended -->
     <template v-if="game.status === 'ended'">
-      <div class="list-group">
+      <h2>🏁 Game over</h2>
+      <h6>Here is the final result.</h6>
+      <div class="list-group mt-4">
         <div v-for="p in players" :key="p.id" class="list-group-item d-flex justify-content-between align-items-center">
           <strong>{{ p.nickname }}</strong>
           <span>{{ p.points }}</span>
