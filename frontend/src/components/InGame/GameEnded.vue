bbd2f0549cb422ecf8778ebd3802482d1065ec3f
Christopher Klaus <hallo@christopherklaus.de>
2021-01-09 22:04:36 +0100

    draft

--- frontend/src/components/InGame/GameEnded.vue
+++ frontend/src/components/InGame/GameEnded.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h2>🏁 Game over</h2>
-    <h6>Here is the final result.</h6>
-    <Leaderboard class="mt-4" />
+  <div class="leaderboard-game-ended">
+    <div class="container">
+      <h2 class="mt-5">🏁 Game over</h2>
+      <!-- add cah headline -->
+      <h6>Here is the final result.</h6>
+      ### podium space (card stack?)
+      <Leaderboard class="mt-4" />
+    </div>
   </div>
 </template>
 
@@ -18,3 +22,14 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="sass">
+.leaderboard-game-ended
+  left: 0
+  top: 0
+  bottom: 0
+  right: 0
+  color: white
+  background-color: black
+  position: fixed
+</style>
