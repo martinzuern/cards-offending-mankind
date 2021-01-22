93501f20a97252329e400f8968b3acb7352e9746
Martin Zürn <hi@martinzuern.de>
2021-01-22 17:50:56 +0100

    Added waiting message for non-hosts before game starts

--- frontend/src/components/InGame/GameCreated.vue
+++ frontend/src/components/InGame/GameCreated.vue
@@ -29,6 +29,7 @@
       >
         Start Game
       </button>
+      <p v-else class="text-center font-weight-bold py-4">Waiting for the host to start the game ...</p>
 
       <h3 class="pt-5 cah-headline color-black">Rules 👩🏻‍⚖️</h3>
       <div class="text-muted mt-3">
