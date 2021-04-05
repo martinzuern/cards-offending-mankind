13b26b3cf9b8bcddd73ba2659208d32310177cfd
Martin Zürn <hi@martinzuern.de>
2021-04-05 17:58:06 +0200

    Added podium

--- frontend/src/components/InGame/GameEnded.vue
+++ frontend/src/components/InGame/GameEnded.vue
@@ -2,14 +2,20 @@
   <div class="leaderboard-game-ended">
     <div class="container">
       <h2 class="mt-5">🏁 Game over</h2>
-      <!-- add cah headline -->
       <h6>Here is the final result.</h6>
-      ### podium space (card stack?) ### what if two people have same amount of points?
-      <div v-for="{ nickname, points } in podiumPlayers" :key="nickname" class="podium-position">
-        <div class="bar"></div>
-        {{ nickname }}
-        {{ points }}
-      </div>
+
+      <b-row class="d-flex podium">
+        <b-col v-for="place in [2, 1, 3]" :key="place" md="4" class="step-container m-0 p-0" :class="`place-${place}`">
+          <div>
+            <b-badge v-for="p in podiumPlayers[place - 1]" :key="p.id" pill>
+              <b-icon icon="award"></b-icon>
+              {{ p.nickname }}
+            </b-badge>
+          </div>
+          <div class="step">{{ place }}</div>
+        </b-col>
+      </b-row>
+
       <Leaderboard class="mt-4" />
     </div>
   </div>
@@ -17,7 +23,8 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { sortBy } from 'lodash';
+import assert from 'assert';
+import { groupBy } from 'lodash';
 
 import { OtherPlayer } from '@/types';
 import store from '@/store';
@@ -29,9 +36,13 @@ export default Vue.extend({
     Leaderboard,
   },
   computed: {
-    podiumPlayers(): OtherPlayer[] {
-      const playersByPoints = sortBy(store.state.gameState.players, ['points']);
-      return playersByPoints.slice(0, 3);
+    podiumPlayers(): OtherPlayer[][] {
+      assert(store.state.gameState);
+      const playersByPoints = groupBy(store.state.gameState.players, 'points');
+      const topPoints = Object.keys(playersByPoints).map(Number).sort().reverse();
+      return Array(3)
+        .fill(0)
+        .map((_val, i) => playersByPoints[topPoints[i]] || []);
     },
   },
 });
@@ -46,4 +57,45 @@ export default Vue.extend({
   color: white
   background-color: black
   position: fixed
+
+  .podium
+    min-height: 18rem
+
+    .step-container
+      flex: 1
+      display: flex
+      flex-direction: column
+
+      &>div:first-child
+        margin-top: auto
+        text-align: center
+        padding: 1rem
+
+        .badge
+          white-space: normal
+          color: black
+
+      .step
+        text-align: center
+        background-color: #383d41
+        font-weight: bold
+        font-size: 4rem
+
+      &.place-1
+        .step
+          height: 60%
+        .badge
+          background-color: #C9B037
+
+      &.place-2
+        .step
+          height: 45%
+        .badge
+          background-color: #D7D7D7
+
+      &.place-3
+        .step
+          height: 40%
+        .badge
+          background-color: #AD8A56
 </style>
